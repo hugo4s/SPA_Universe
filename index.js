82abc5fcdfe3c6464ffcc1cf2cac5b0b17a5e88c
hugo4s <140451515+hugo4s@users.noreply.github.com>
2023-10-14 18:17:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const route = (event) => {
+    event = event || window.event
+    event.preventDefault()
+    window.history.pushState({}, "", event.target.href)
+    handleLocation()
+}
+
+const routes = {
+    "/": "/home.html",
+    "/universe": "/universe.html",
+    "/explorer": "/explorer.html",
+}
+const handleLocation = async () => {
+    const path = window.location.pathname
+    const route = routes[path]
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("app").innerHTML = html
+}
+
+window.onpopstate = handleLocation
+
+window.route = route
+handleLocation()
